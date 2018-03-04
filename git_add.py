d2c37b49f84a8f13fc37877d24c18e0f9c058de2
R4mble <sanshi.30@qq.com>
2018-03-04 22:31:23 +0800

    “update”

--- git_add.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: cp936 -*-
-
-import os
-
-#os.system("cd ../.deploy/qiyueji.github.io")
-os.system("git add .")
-#os.system("echo git commit -m ¡°update¡±")
-
--- git_push.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-#os.system("cd ../.deploy/qiyueji.github.io")
-os.system("git push -f origin master")
